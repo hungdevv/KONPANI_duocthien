ced98c8b5054499dbb80a8a5faba6843f6aef5c5
Hung <hung@example.com>
2021-01-22 14:49:01 +0700

    api

--- application/controllers/Cdatalinkage.php
+++ application/controllers/Cdatalinkage.php
@@ -39,6 +39,42 @@ class Cdatalinkage extends CI_Controller {
         // $content = $this->lcustomer->customer_edit_data($customer_id);
         // $this->template->full_admin_html_view($content);
     }   
+
+    public function medication_list_BYT1() {
+        $CI =& get_instance();
+        $this->auth->check_admin_auth();
+        $CI->load->library('ldatalinkage');
+        $CI->load->model('Datalinkages');
+        $content =$this->ldatalinkage->datalinkage_list();
+        $this->template->full_admin_html_view($content);
+    }
+    public function count_datalinkage() {
+        return $this->db->count_all("medication_list_byt");
+    }
+
+    
+    public function CheckDatalinkageList(){
+        // GET data
+        $this->load->model('Datalinkages');
+        $postData = $this->input->post();
+        $data = $this->Datalinkages->getDatalinkageList($postData);
+        echo json_encode($data);
+    } 
+
+    public function manage_datalinkage() {
+        $CI =& get_instance();
+        $this->auth->check_admin_auth();
+        $CI->load->library('ldatalinkage');
+        $CI->load->model('Datalinkages');
+        $content =$this->ldatalinkage->datalinkage_list();
+        $this->template->full_admin_html_view($content);
+    }
+
+    public function datalinkage_update_form($customer_id) {
+        $content = $this->lcustomer->customer_edit_data($customer_id);
+        $this->template->full_admin_html_view($content);
+    }
+
     // public function credit_customer_search_item() {
     //     $customer_id = $this->input->post('customer_id',true);
     //     $content = $this->lcustomer->credit_customer_search_item($customer_id);
--- application/libraries/Ldatalinkage.php
+++ application/libraries/Ldatalinkage.php
@@ -7,32 +7,33 @@ class Ldatalinkage {
 
 
 
-    public function customer_list(){
+    public function datalinkage_list(){
         $CI =& get_instance();
-        $CI->load->model('Customers');
+        $CI->load->model('Datalinkages');
         $CI->load->model('Web_settings');
-        $company_info = $CI->Customers->retrieve_company();
-        $currency_details = $CI->Web_settings->retrieve_setting_editdata();
-        $data['total_customer']    = $CI->Customers->count_customer();
+        $company_info = $CI->Datalinkages->retrieve_company();
+        $currency_details = $CI->Web_settings->retrieve_setting_editdata(); 
+        // $data['total_customer']    = $CI->Datalinkages->count_datalinkage();
         $data['currency']          = $currency_details[0]['currency'];
         $data['title']             = display('manage_customer');
         $data['company_info']      = $company_info;
-        $customerList = $CI->parser->parse('datalinkage/index',$data,true);
-        return $customerList;
+        $datalinkageList = $CI->parser->parse('datalinkage/index',$data,true);
+        return $datalinkageList;
     }
 
     //Retrieve  Credit Customer List	
-    public function credit_customer_list() {
-        $CI = & get_instance();
-        $CI->load->model('Customers');
-        $CI->load->model('Web_settings');
-        $company_info = $CI->Customers->retrieve_company();
-        $currency_details = $CI->Web_settings->retrieve_setting_editdata();
-        $data['currency']       = $currency_details[0]['currency'];
-        $data['company_info']   = $company_info;
-        $data['total_customer'] = $CI->Customers->count_credit_customer();
-        $customerList = $CI->parser->parse('customer/credit_customer', $data, true);
-        return $customerList;
-    }
+
+    // public function credit_customer_list() {
+    //     $CI = & get_instance();
+    //     $CI->load->model('Datalinkage');
+    //     $CI->load->model('Web_settings');
+    //     $company_info = $CI->Customers->retrieve_company();
+    //     $currency_details = $CI->Web_settings->retrieve_setting_editdata();
+    //     $data['currency']       = $currency_details[0]['currency'];
+    //     $data['company_info']   = $company_info;
+    //     $data['total_customer'] = $CI->Customers->count_credit_customer();
+    //     $customerList = $CI->parser->parse('customer/credit_customer', $data, true);
+    //     return $customerList;
+    // }
 }
 ?>
